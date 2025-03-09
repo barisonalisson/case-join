7b5e5a8deeefb2df13ebc2693274c7ad4ad0a4f6
barisonalisson <barisonalisson@gmail.com>
2025-03-09 05:18:15 -0400

    add CategoriaProduto and Produto model relationships and define table properties

--- app/Models/CategoriaProduto.php
+++ app/Models/CategoriaProduto.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoriaProduto extends Model
 {
-    //
+    protected $table = 'tb_categoria_produto';
+    protected $primaryKey = 'id_categoria_planejamento';    
+    protected $fillable = ['nome_categoria'];
+
+    public function produtos()
+    {
+        return $this->hasMany(Produto::class, 'id_categoria_produto', 'id_categoria_planejamento');
+    }
 }
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $table = 'tb_produto';
+    protected $primaryKey = 'id_produto';
+    protected $fillable = ['id_categoria_produto', 'nome_produto', 'valor'];
+
+    public function categoria()
+    {
+        return $this->belongsTo(CategoriaProduto::class, 'id_categoria_produto', 'id_categoria_planejamento');
+    }
 }
